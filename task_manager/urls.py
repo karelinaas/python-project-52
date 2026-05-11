006762b4eac50bd6b787979ecf6f669f56bf71ea
karelinaas <karelinaas95@gmail.com>
2026-05-11 17:26:56 +0300

    Step 3 - Lint fix

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -6,5 +6,5 @@ from task_manager import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("admin/", admin.site.urls),
-    path("users/", include('users.urls')),
+    path("users/", include("users.urls")),
 ]
--- task_manager/views.py
+++ task_manager/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 
 def home(request: HttpRequest):
-    return render(request, 'home.html')
+    return render(request, "home.html")
